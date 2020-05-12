58738d2ada76aac317a503fcbffd307564513d1d
swuuws <swuuws@yeah.net>
2020-05-12 10:24:04 +0800

    Create T.php

---
+++ src/swuuws/T.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * Project: swuuws
+ * Producer: swuuws [ http://www.swuuws.com ]
+ * Author: A.J <804644245@qq.com>
+ * Copyright: http://www.swuuws.com All rights reserved.
+ */
+namespace swuuws;
+
+class T
+{
+    public static function jpg()
+    {
+        return 'image/jpeg';
+    }
+    public static function gif()
+    {
+        return 'image/gif';
+    }
+    public static function png()
+    {
+        return 'image/png';
+    }
+    public static function svg()
+    {
+        return 'image/svg+xml';
+    }
+    public static function ico()
+    {
+        return 'image/x-icon';
+    }
+    public static function bmp()
+    {
+        return 'image/bmp';
+    }
+    public static function mp3()
+    {
+        return 'audio/mpeg';
+    }
+    public static function zip()
+    {
+        return 'application/zip';
+    }
+    public static function gz()
+    {
+        return 'application/x-gzip';
+    }
+    public static function tgz()
+    {
+        return 'application/x-compressed';
+    }
+    public static function pdf()
+    {
+        return 'application/pdf';
+    }
+    public static function doc()
+    {
+        return 'application/msword';
+    }
+    public static function xls()
+    {
+        return 'application/vnd.ms-excel';
+    }
+    public static function ppt()
+    {
+        return 'application/vnd.ms-powerpoint';
+    }
+    public static function gtar()
+    {
+        return 'application/x-gtar';
+    }
+    public static function swf()
+    {
+        return 'application/x-shockwave-flash';
+    }
+    public static function wav()
+    {
+        return 'audio/x-wav';
+    }
+    public static function txt()
+    {
+        return 'text/plain';
+    }
+    public static function mp4()
+    {
+        return 'video/mp4';
+    }
+    public static function flv()
+    {
+        return 'video/x-flv';
+    }
+    public static function ogg()
+    {
+        return 'audio/ogg';
+    }
+}
