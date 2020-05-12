f1892377c831aba180585b7c0698bdbbbc090eac
swuuws <swuuws@yeah.net>
2020-05-12 10:22:03 +0800

    Create M.php

---
+++ src/swuuws/M.php
@@ -0,0 +1,112 @@
+<?php
+/**
+ * Project: swuuws
+ * Producer: swuuws [ http://www.swuuws.com ]
+ * Author: A.J <804644245@qq.com>
+ * Copyright: http://www.swuuws.com All rights reserved.
+ */
+namespace swuuws;
+
+class M
+{
+    public static function tinyint()
+    {
+        return 'tinyint';
+    }
+    public static function smallint()
+    {
+        return 'smallint';
+    }
+    public static function mediumint()
+    {
+        return 'mediumint';
+    }
+    public static function int()
+    {
+        return 'int';
+    }
+    public static function bigint()
+    {
+        return 'bigint';
+    }
+    public static function float()
+    {
+        return 'float';
+    }
+    public static function double()
+    {
+        return 'double';
+    }
+    public static function decimal()
+    {
+        return 'decimal';
+    }
+    public static function real()
+    {
+        return 'real';
+    }
+    public static function boolean()
+    {
+        return 'boolean';
+    }
+    public static function date()
+    {
+        return 'date';
+    }
+    public static function time()
+    {
+        return 'time';
+    }
+    public static function year()
+    {
+        return 'year';
+    }
+    public static function datetime()
+    {
+        return 'datetime';
+    }
+    public static function timestamp()
+    {
+        return 'timestamp';
+    }
+    public static function char()
+    {
+        return 'char';
+    }
+    public static function varchar()
+    {
+        return 'varchar';
+    }
+    public static function tinyblob()
+    {
+        return 'tinyblob';
+    }
+    public static function tinytext()
+    {
+        return 'tinytext';
+    }
+    public static function blob()
+    {
+        return 'blob';
+    }
+    public static function text()
+    {
+        return 'text';
+    }
+    public static function mediumblob()
+    {
+        return 'mediumblob';
+    }
+    public static function mediumtext()
+    {
+        return 'mediumtext';
+    }
+    public static function longblob()
+    {
+        return 'longblob';
+    }
+    public static function longtext()
+    {
+        return 'longtext';
+    }
+}
