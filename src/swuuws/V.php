70db17c1b029fa87f7ee27abc06d7c74c6a58a46
swuuws <swuuws@yeah.net>
2020-05-12 10:24:31 +0800

    Create V.php

---
+++ src/swuuws/V.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Project: swuuws
+ * Producer: swuuws [ http://www.swuuws.com ]
+ * Author: A.J <804644245@qq.com>
+ * Copyright: http://www.swuuws.com All rights reserved.
+ */
+namespace swuuws;
+
+class V
+{
+    public static function must()
+    {
+        return 'must';
+    }
+    public static function int()
+    {
+        return 'integer';
+    }
+    public static function bool()
+    {
+        return 'boolean';
+    }
+    public static function float()
+    {
+        return 'float';
+    }
+    public static function url()
+    {
+        return 'url';
+    }
+    public static function email()
+    {
+        return 'email';
+    }
+    public static function ip()
+    {
+        return 'ip';
+    }
+    public static function number()
+    {
+        return 'number';
+    }
+    public static function date()
+    {
+        return 'date';
+    }
+    public static function regexp()
+    {
+        return 'regexp';
+    }
+    public static function letter()
+    {
+        return 'letter';
+    }
+    public static function letterUnder()
+    {
+        return 'letterUnder';
+    }
+    public static function letterHyphen()
+    {
+        return 'letterHyphen';
+    }
+    public static function letterHyphenUnder()
+    {
+        return 'letterHyphenUnder';
+    }
+    public static function captcha()
+    {
+        return 'captcha';
+    }
+    public static function maxlen()
+    {
+        return 'maxlen';
+    }
+    public static function minlen()
+    {
+        return 'minlen';
+    }
+}
