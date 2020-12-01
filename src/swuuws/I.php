63a9897f5e740b6293e94d48158c63d824694260
yanzicms <804644245@qq.com>
2020-12-01 21:27:30 +0800

    Create I.php

---
+++ src/swuuws/I.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Project: swuuws
+ * Producer: swuuws [ http://www.swuuws.com ]
+ * Author: A.J <804644245@qq.com>
+ * Copyright: http://www.swuuws.com All rights reserved.
+ */
+namespace swuuws;
+
+class I
+{
+    public static function center()
+    {
+        return 'center';
+    }
+    public static function centerLeft()
+    {
+        return 'centerleft';
+    }
+    public static function centerRight()
+    {
+        return 'centerright';
+    }
+    public static function centerTop()
+    {
+        return 'centertop';
+    }
+    public static function centerBottom()
+    {
+        return 'centerbottom';
+    }
+    public static function leftTop()
+    {
+        return 'lefttop';
+    }
+    public static function rightTop()
+    {
+        return 'righttop';
+    }
+    public static function leftBottom()
+    {
+        return 'leftbottom';
+    }
+    public static function rightBottom()
+    {
+        return 'rightbottom';
+    }
+}
